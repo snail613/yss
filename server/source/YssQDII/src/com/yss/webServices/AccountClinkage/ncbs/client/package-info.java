90fc5605e08bfb43b9fdbab7e067017990fd55a2
snail613 <snail613@163.com>
2018-03-27 17:49:11 +0800

    yss init

---
+++ server/source/YssQDII/src/com/yss/webServices/AccountClinkage/ncbs/client/package-info.java
@@ -0,0 +1,8 @@
+
+
+/**
+ *add by huangqirong 2013-05-09 story #3871 需求北京-[建设银行]QDII系统[高]20130419001 
+ * */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://inf.webservice.service.ncbs.ccb.com/")
+package com.yss.webServices.AccountClinkage.ncbs.client;
+
